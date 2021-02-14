c5ac941444ad6956d1bedd1d2c36ed321ed742ee
Stephen Akuffo <sa.akuffo@gmail.com>
2021-02-13 20:48:21 -0500

    ch4

---
+++ ch3-ch4-setup/backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "gcs" {
+    bucket      = "saa-gcp-tf-acg-01"
+    prefix      = "terraform1"
+    credentials = "gcp-tf.json"
+  }
+}
--- ch3-setup/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-    backend "gcs" {
-        bucket = "saa-gcp-tf-acg-01"
-        prefix = "terraform1"
-        credentials = "gcp-tf.json"
-    }
-}

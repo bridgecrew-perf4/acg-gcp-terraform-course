7ccb4154ec6eae02dba94838640bd6bd833793b4
Stephen Akuffo <sa.akuffo@gmail.com>
2021-02-13 20:38:41 -0500

    ch3

--- ch3-setup/backend.tf
+++ ch3-setup/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "gcs" {
-        bucket = "saa-gcp-tf-acg"
+        bucket = "saa-gcp-tf-acg-01"
         prefix = "terraform1"
         credentials = "gcp-tf.json"
     }
--- ch3-setup/main.tf
+++ ch3-setup/main.tf
@@ -1,6 +1,6 @@
 provider "google" {
-  credentials = file("~/.ssh/playground-s-11-15f029c2-746d50b36343.json")
-  project = "playground-s-11-15f029c2"
+  credentials = file("gcp-tf.json")
+  project = "playground-s-11-58f1561b"
   region = "us-central1"
   zone = "us-central1-c"
 }
